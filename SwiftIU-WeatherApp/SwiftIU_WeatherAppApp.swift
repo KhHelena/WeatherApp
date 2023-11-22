d5b3417091656bbbdb598c2a9c36bb69fbbf4ab7
KhHelena <lstudyf@gmail.com>
2023-11-22 23:53:58 +0200

    Initial Commit

---
+++ SwiftIU-WeatherApp/SwiftIU_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIU_WeatherAppApp.swift
+//  SwiftIU-WeatherApp
+//
+//  Created by Macbook on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIU_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
